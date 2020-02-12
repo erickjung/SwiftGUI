3f734c3466151f82e9298bb86ca9b2369a5a06d1
Erick Jung <erick.jung@farfetch.com>
2020-02-12 12:14:48 +0000

    adding listbuffer and new foreach

--- SwiftGui/SwiftGui/Nodes/Common.swift
+++ SwiftGui/SwiftGui/Nodes/Common.swift
@@ -55,17 +55,28 @@ public func HStack(@GuiBuilder child: () -> GuiView?) -> GuiNode {
 
 public func ForEach<T:Sequence>(_ data: T,
                                 @GuiBuilder child: (T.Element) -> GuiView?) -> GuiView {
-    
+
     var list = [GuiView]()
     for (index, element) in data.enumerated() {
         list.append(GuiNode(tag: "ForEach_\(index)", child: child(element)).onRender { child in
-            
+
             child?.render()
         })
     }
     return GuiMultiNode(children: list)
 }
 
+public func ForEach2<T:Sequence>(_ data: T,
+                                 onLoop: @escaping (T.Element) -> GuiNode?) -> GuiView {
+    
+    GuiNode(tag: #function).onRender { _ in
+
+        for (_, element) in data.enumerated() {
+            
+            onLoop(element)?.render()
+        }
+    }
+}
 
 public func Separator() -> GuiNode {
     
